e804ba4956638e6acb9e462c9a17189d29c82350
glx513 <guo.lingx@northeastern.edu>
2024-07-29 20:44:09 -0700

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from accounts.forms import UserForm
-from accounts.models import User
+from accounts.models import User, UserProfile
 from django.contrib import messages
+
+from vendor.forms import VendorForm
 
 
 # Create your views here.
@@ -51,6 +53,42 @@ def registerUser(request):
     # context 是一个字典，字典中的键是模板中可以访问的数据名称，值是实际的数据或对象。
     # 这里的 form 是一个表单对象，它将被传递到 registerUser.html 模板中，以便在页面上渲染和显示。
     context = {
-        'form':form,
+        'form': form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+def registerVendor(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email,
+                                            password=password)
+            user.role = User.VENDOR
+            user.save()
+
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            # vendor_name = v_form.cleaned_data['vendor_name']
+            # vendor.vendor_slug = slugify(vendor_name) + '-' + str(user.id)
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Your account has been registered successfully!')
+            return redirect('registerVendor')
+        else:
+            print("invalid form")
+            print(form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+    context = {
+        'form': form,
+        'v_form' : v_form
+    }
+    return render(request, 'accounts/registerVendor.html', context)
