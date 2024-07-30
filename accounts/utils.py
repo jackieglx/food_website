f82e89af69c859f00db3100e4897969e2f627859
glx513 <guo.lingx@northeastern.edu>
2024-07-30 13:20:08 -0700

    update forgot password function

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from foodOnline import settings
+
+
 def detectUser(user):
     if user.role == 1:
         redirectUrl = 'vendorDashboard'
@@ -8,3 +18,21 @@ def detectUser(user):
     elif user.role == None and user.is_superuser:
         redirectUrl = '/admin'
         return redirectUrl
+
+# 这个函数用于向用户发送验证邮件。
+# 它会生成一封包含用户信息的 HTML 邮件，并通过指定的邮箱服务器发送给用户。
+# 邮件的内容包括一个包含用户唯一标识符和安全令牌的链接，通常用于用户验证或密码重置。
+def send_verification_email(request, user, mail_subject, email_template):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    current_site = get_current_site(request)
+    message = render_to_string(email_template, {
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': default_token_generator.make_token(user),
+    })
+    to_email = user.email
+    mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.content_subtype = "html"
+    mail.send()
+
